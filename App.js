97bff22d63df00c34d5e0b2fc993d74ef49e20b9
Simrat Hayer <hayersimrat23@gmail.com>
2024-09-24 15:08:48 -0400

    testing all lazy

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import MyStack from './Frontend/MyStack';
+import MyStack from './Frontend/MyStack_AllLazy';
 
 const App = () => {
   return (
