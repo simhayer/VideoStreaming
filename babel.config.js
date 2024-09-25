86f7f3e897c061cc3ba714a1c1d56658c4116746
Simrat Hayer <hayersimrat23@gmail.com>
2024-09-25 12:00:27 -0400

    production constraints for ios

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,11 @@ require('dotenv').config();
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: ['react-native-reanimated/plugin'],
+
+  presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'], // Add this line
+    },
+  },
 };
